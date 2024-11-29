da2493f476d8af80a9a5400cd204546f0b4915d1
Korhelyleves <ooheey@proton.me>
2024-11-29 10:58:30 +0100

    Fix dap/root patch

--- patches/src/main/kotlin/app/revanced/patches/dap/restrictions/root/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/dap/restrictions/root/Fingerprints.kt
@@ -1,9 +1,10 @@
 package app.revanced.patches.dap.restrictions.root
 
+import com.android.tools.smali.dexlib2.AccessFlags
 import app.revanced.patcher.fingerprint
 
 internal val checkRootFingerprint = fingerprint {
-    custom { method, _ ->
-        method.name == "<init>" && method.definingClass == "Lut;"
-    }
+    accessFlags(AccessFlags.PUBLIC, AccessFlags.CONSTRUCTOR)
+    returns("V")
+    parameters("Z","Z","Z","Z","Z","Z","Z","Z","Z")
 }
